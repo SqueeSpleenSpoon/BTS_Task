aa8804083baaca765fdf8a3408bbaa1e3d5a405a
Katherine Kt <kt.kaminskaya@gmail.com>
2021-04-23 16:31:34 +0300

    UnitTest booking

--- BookingTable.Tests/UnitTest1.cs
+++ BookingTable.Tests/UnitTest1.cs
@@ -2,17 +2,34 @@ using NUnit.Framework;
 
 namespace BookingTable.Tests
 {
-    public class Tests
+    public class TableTest
     {
         [SetUp]
         public void Setup()
         {
+
         }
 
         [Test]
-        public void Test1()
+        public void TableTest_BookingMethod()
         {
-            Assert.Pass();
+            var table = new Table();
+            var table2 = new Table();
+
+            table.AddTable(table2);
+            table2.BookedTime = 10;
+            table.BookedTime = 10;
+
+            var findTable = table.TableList.Find(table => table.BookedTime != table2.BookedTime);
+
+
+            //table.TableList.Find(table2, table.TableList);
+            //var findTable = TableList.Find(table => table.BookedTime != bookedTime);
+
+            //var inputTime = 13;
+
+
+            Assert.IsNull(findTable);
         }
     }
 }
