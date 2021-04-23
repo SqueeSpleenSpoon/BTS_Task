092513848a6553076b7c44b1e04159d490f0c7ff
Katherine Kt <kt.kaminskaya@gmail.com>
2021-04-23 15:54:46 +0300

    Created Test project

---
+++ BookingTable.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace BookingTable.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
